581557eee8b6a64dcf5332c208305fb28d57960c
김우재 <kwj1270@naver.com>
2021-03-29 23:02:29 +0900

    test:회원 도메인 실행과 테스트

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    private MemberService memberService = new MemberServiceImpl();
+
+    @DisplayName("MemberService의 join 메서드 테스트")
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        assertThat(member).isEqualTo(findMember);
+    }
+}
