f77428f6e0463e131a5b345198a0985fe6c38d87
김우재 <kwj1270@naver.com>
2021-04-11 16:20:45 +0900

    refactor:상태를 가진 잘못된 싱글턴 클래스가 상태를 가지지 않도록 리팩토링

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,14 +2,8 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price;  // 상태가 유지되는 변수
-
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 문제가 되는 영역
-    }
-
-    public int getPrice() {
         return price;
     }
 
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -15,17 +15,13 @@ class StatefulServiceTest {
         StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
         StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
 
-        statefulService1.order("userA", 10000);
-        statefulService1.order("userB", 20000);
-
-        //ThreadA : 사용자 주문 금액 조회
-        int priceA = statefulService1.getPrice();
-        int priceB = statefulService1.getPrice();
+        int priceA = statefulService1.order("userA", 10000);
+        int priceB =  statefulService1.order("userB", 20000);
 
         System.out.println("priceA = " + priceA);   // 값이 예상과 다르게 나온다.
         System.out.println("priceB = " + priceB);
 
-        assertThat(priceA).isEqualTo(priceB); // 잘못된 테스트 케이스까지 통과된다.
+        assertThat(priceA).isNotEqualTo(priceB); // 잘못된 테스트 케이스까지 통과된다.
     }
 
     static class TestConfig {
