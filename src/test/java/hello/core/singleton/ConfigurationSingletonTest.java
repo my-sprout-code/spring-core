b5bad949d5128c074b6c2b6e6ea7c7dd80391725
김우재 <kwj1270@naver.com>
2021-04-11 21:20:11 +0900

    feat:@Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -2,9 +2,7 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -36,5 +34,14 @@ public class ConfigurationSingletonTest {
                 () -> assertThat(memberRepository1).isSameAs(memberRepository2)
         );
 
+    }
+
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
     }
 }
